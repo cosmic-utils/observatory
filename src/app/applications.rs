b001e0bf0e7dc60060db815463c3cf6af34c22eb
Adam Cosner <adam.cosner@proton.me>
2024-11-16 12:03:29 -0800

    Fixed an issue on some platforms where applications.rs would cause a crash on launch

--- src/app/applications.rs
+++ src/app/applications.rs
@@ -20,35 +20,35 @@ impl Application {
         for file in files {
             let file = file.unwrap();
             let path = file.path();
-            let desktop_entry = parse_entry(&path).unwrap();
+            if let Ok(desktop_entry) = parse_entry(&path) {
+                let name = if let Some(name) = desktop_entry.section("Desktop Entry").attr("Name") {
+                    name.to_string()
+                } else {
+                    path.file_name().expect("Could not read file name of application!").to_str().unwrap().to_string()
+                };
 
-            let name = if let Some(name) = desktop_entry.section("Desktop Entry").attr("Name") {
-                name.to_string()
-            } else {
-                path.file_name().unwrap().to_str().unwrap().to_string()
-            };
+                // Used for clearing multiple cmd situations
+                let full_cmd = if let Some(cmd) = desktop_entry.section("Desktop Entry").attr("Exec") {
+                    cmd.to_string()
+                } else {
+                    "".to_string()
+                };
 
-            // Used for clearing multiple cmd situations
-            let full_cmd = if let Some(cmd) = desktop_entry.section("Desktop Entry").attr("Exec") {
-                cmd.to_string()
-            } else {
-                "".to_string()
-            };
+                let cmd = if let Some(cmd) = desktop_entry.section("Desktop Entry").attr("Exec") {
+                    cmd.to_string().split(" ").nth(0).unwrap().to_string()
+                } else {
+                    "".to_string()
+                };
 
-            let cmd = if let Some(cmd) = desktop_entry.section("Desktop Entry").attr("Exec") {
-                cmd.to_string().split(" ").nth(0).unwrap().to_string()
-            } else {
-                "".to_string()
-            };
+                let icon = if let Some(icon) = desktop_entry.section("Desktop Entry").attr("Icon") {
+                    icon.to_string()
+                } else {
+                    "application-default-symbolic".to_string()
+                };
 
-            let icon = if let Some(icon) = desktop_entry.section("Desktop Entry").attr("Icon") {
-                icon.to_string()
-            } else {
-                "application-default-symbolic".to_string()
-            };
-
-            if full_cmd.cmp(&cmd) == Ordering::Equal || !apps.contains_key(&cmd) {
-                apps.insert(cmd.clone(), Application { _name: name, cmd, icon });
+                if full_cmd.cmp(&cmd) == Ordering::Equal || !apps.contains_key(&cmd) {
+                    apps.insert(cmd.clone(), Application { _name: name, cmd, icon });
+                }
             }
         }
 

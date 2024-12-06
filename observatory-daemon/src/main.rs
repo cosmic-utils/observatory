fb8627c6f13f06e19ce8ed9e2884e4813a8f4d63
Adam Cosner <adam.cosner@proton.me>
2024-12-06 07:36:13 -0800

    Write to my desktop

--- observatory-daemon/src/main.rs
+++ observatory-daemon/src/main.rs
@@ -1,6 +1,10 @@
+use std::io::Write;
+
 fn main() {
     let paths = std::fs::read_dir("/proc").unwrap();
+    let mut out = std::fs::File::create("/home/adamc/Desktop/processes.txt").unwrap();
+
     for path in paths {
-        println!("{}", path.unwrap().path().display());
+        out.write_all(format!("{}", path.unwrap().path().display()).as_bytes()).unwrap();
     }
 }

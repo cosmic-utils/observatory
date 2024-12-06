27198424cd295f865c9a2eb7aebb40260cd487f4
Adam Cosner <adam.cosner@proton.me>
2024-12-06 04:45:56 -0800

    Flatpak unsandboxed

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,9 @@ mod pages;
 mod widgets;
 
 fn main() -> cosmic::iced::Result {
+    if flatpak_unsandbox::unsandbox(None).is_ok() {
+        return Ok(())
+    }
     settings::init();
-    log::warn!("{}", std::str::from_utf8(std::process::Command::new("flatpak-spawn").arg("--host").arg("\"ps -e\"").output().unwrap().stdout.as_slice()).unwrap());
     cosmic::app::run::<app::App>(settings::settings(), settings::flags())
 }

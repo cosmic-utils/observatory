8e33cca37e8071854ed0862bb96212229fd4ac9a
Adam Cosner <adam.cosner@proton.me>
2024-12-06 07:08:54 -0800

    fix compile error

--- observatory/src/main.rs
+++ observatory/src/main.rs
@@ -7,7 +7,7 @@ mod widgets;
 
 fn main() -> cosmic::iced::Result {
     let command = std::process::Command::new("flatpak-spawn").arg("observatory-daemon").output();
-    if let Some(output) = command {
+    if let Ok(output) = command {
         println!("{}", String::from_utf8(output.stdout).unwrap());
     } else {
         println!("Could not flatpak-spawn!");

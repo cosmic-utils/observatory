919cd638dcf93e2fd968e67c8889ca116c1ed9b3
Adam Cosner <adam.cosner@proton.me>
2024-12-06 05:13:10 -0800

    Removed flatpak  building ability since flatpak is not the proper distribution platform for this

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,6 @@ mod pages;
 mod widgets;
 
 fn main() -> cosmic::iced::Result {
-    if flatpak_unsandbox::unsandbox(None).is_ok() {
-        return Ok(())
-    }
     settings::init();
     cosmic::app::run::<app::App>(settings::settings(), settings::flags())
 }

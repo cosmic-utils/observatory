d8a40d7ac8ab5f52e9813e4e2c217c3077061476
Adam Cosner <adam.cosner@proton.me>
2024-12-06 03:49:32 -0800

    Work on flatpak build
    CURRENTLY SPAMS THE CONSOLE WITH MESSAGES LISTING /proc FILES, THIS WILL BE REMOVED ONCE FLATPAK IS WORKING

--- src/main.rs
+++ src/main.rs
@@ -7,5 +7,16 @@ mod widgets;
 
 fn main() -> cosmic::iced::Result {
     settings::init();
+    let paths = std::fs::read_dir("/proc/");
+    match paths {
+        Ok(paths) => {
+            for path in paths {
+                log::warn!("{:?}", path);
+            }
+        }
+        Err(e) => {
+            log::error!("Failed to read /proc/: {}", e);
+        }
+    }
     cosmic::app::run::<app::App>(settings::settings(), settings::flags())
 }

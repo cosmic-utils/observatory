c54cbb041eeadf0e18e5a2965db37e4f0e18563d
Adam Cosner <adam.cosner@proton.me>
2024-12-06 06:44:11 -0800

    Revert "Removed flatpak  building ability since flatpak is not the proper distribution platform for this"
    This reverts commit 919cd638dcf93e2fd968e67c8889ca116c1ed9b3.

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,9 @@ mod pages;
 mod widgets;
 
 fn main() -> cosmic::iced::Result {
+    if flatpak_unsandbox::unsandbox(None).is_ok() {
+        return Ok(())
+    }
     settings::init();
     cosmic::app::run::<app::App>(settings::settings(), settings::flags())
 }

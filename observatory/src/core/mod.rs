9ae9eda8d0e4df25397bbb2f33b7f9a2b9e853b8
Adam Cosner <adam.cosner@proton.me>
2025-01-14 22:20:28 -0800

    Removed old version

--- observatory/src/core/mod.rs
+++
@@ -1,5 +0,0 @@
-pub mod config;
-pub mod i18n;
-pub mod icons;
-pub mod settings;
-pub mod system_info;
--- observatory/src/widgets/mod.rs
+++
@@ -1,5 +0,0 @@
-pub mod line_graph;
-pub mod meter;
-
-pub use line_graph::LineGraph;
-pub use meter::Meter;

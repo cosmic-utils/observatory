36544302c3a754ef9f0649cc5637e0c901ad000d
Adam Cosner <adam.cosner@proton.me>
2025-01-14 23:24:42 -0800

    Initial monitord zbus interface established

--- monitord/src/main.rs
+++ monitord/src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+mod system;
+use system::SystemServerSignals;
+
+#[tokio::main]
+async fn main() -> zbus::Result<()> {
+    let system_server = system::SystemServer {};
+    let connection = zbus::connection::Builder::session()?
+        .name("io.github.CosmicUtils.Observatory")?
+        .serve_at("/io/github/CosmicUtils/Observatory", system_server)?
+        .build()
+        .await?;
+
+    loop {
+        std::thread::sleep(std::time::Duration::from_secs(1));
+        connection
+            .object_server()
+            .interface("/io/github/CosmicUtils/Observatory")
+            .await?
+            .snapshot(system::SystemSnapshot {
+                processes: Vec::new(),
+            })
+            .await?;
+    }
 }
---
+++ monitord/src/system/process.rs
@@ -0,0 +1,4 @@
+#[derive(zbus::zvariant::Type, serde::Serialize, serde::Deserialize)]
+pub struct Process {
+    // TODO
+}

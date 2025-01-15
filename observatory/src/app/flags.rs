7f87253b3dacae3fb35f4f4c9f9d6dfdf814af61
Adam Cosner <adam.cosner@proton.me>
2025-01-14 22:20:28 -0800

    Removed old version

--- observatory/src/app/flags.rs
+++
@@ -1,4 +0,0 @@
-#[derive(Default)]
-pub struct Flags {
-    // empty for a reason
-}
--- observatory/src/pages/mod.rs
+++
@@ -1,7 +0,0 @@
-pub mod page;
-
-pub mod overview;
-pub mod processes;
-pub mod resources;
-
-pub use page::Page;

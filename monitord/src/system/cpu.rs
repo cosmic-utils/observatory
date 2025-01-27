a608bcbac7b6af4c7aca284641dd6101f466158b
Adam Cosner <adam.cosner@proton.me>
2025-01-27 15:45:22 -0800

    feat: CPU Cache recording in monitord

--- monitord/src/system/cpu.rs
+++ monitord/src/system/cpu.rs
@@ -3,14 +3,42 @@ lazy_static::lazy_static!(
 );
 
 #[derive(zbus::zvariant::Type, serde::Serialize, serde::Deserialize, Debug, Clone)]
+pub struct CpuCache {
+    pub size: usize,
+    pub level: u8,
+    pub cache_type: String,
+}
+
+#[derive(zbus::zvariant::Type, serde::Serialize, serde::Deserialize, Debug, Clone)]
 pub struct CpuStatic {
     // The model name of the CPU
     pub model: String,
     pub physical_cores: usize,
     pub logical_cores: usize,
+    pub caches: Vec<CpuCache>,
 }
 
 impl CpuStatic {
+    fn load_cache(cpuid: &raw_cpuid::CpuId<raw_cpuid::CpuIdReaderNative>) -> Vec<CpuCache> {
+        if let Some(intel_cache) = cpuid.get_cache_parameters() {
+            let mut caches = Vec::new();
+            for cache in intel_cache {
+                let size = cache.associativity()
+                    * cache.physical_line_partitions()
+                    * cache.coherency_line_size()
+                    * cache.sets();
+                caches.push(CpuCache {
+                    size,
+                    level: cache.level(),
+                    cache_type: cache.cache_type().to_string(),
+                })
+            }
+            caches
+        } else {
+            Vec::new()
+        }
+    }
+
     fn load() -> Self {
         let cpuid = raw_cpuid::CpuId::new();
         Self {
@@ -21,6 +49,7 @@ impl CpuStatic {
                 .to_string(),
             physical_cores: num_cpus::get_physical(),
             logical_cores: num_cpus::get(),
+            caches: Self::load_cache(&cpuid),
         }
     }
 }
